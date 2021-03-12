665394d9901d1b1371ab3947ee2d6bad058bbc82
JotaeMeZe <jmmoralesc03@iesalbarregas.es>
2021-03-12 18:08:33 +0100

    Líneas en método main

--- Jesus_Manuel_Morales_DAW/src/Jesus_Manuel_Morales_DAW.java
+++ Jesus_Manuel_Morales_DAW/src/Jesus_Manuel_Morales_DAW.java
@@ -3,6 +3,8 @@ public class Jesus_Manuel_Morales_DAW {
 
 	public static void main(String[] args) {
 		System.out.println("Primera línea de nuestro repositorio");
+		System.out.println("Añado una línea en el método Main de mi clase Principal, en mi"
+				+ " caso se trata de la clase Jesus_Manuel_Morales_Campos");
 	}
 
 }
